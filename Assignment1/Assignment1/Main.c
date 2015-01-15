f03c18b3100e2785c41a8045d372eb689e9d42ae
falb0005 <john_falbo31@live.ca>
2015-01-15 09:17:38 -0500

    Added basic asteroid implementation
    have points representing asteroird moving

--- Assignment1/Assignment1/Main.c
+++ Assignment1/Assignment1/Main.c
@@ -1,9 +1,14 @@
 #include <SDL.h>
 #include <stdio.h>
+#include "Asteroids.h"
+
+struct Asteroid * asteroid_head = NULL;
+SDL_Window * window = NULL;
+SDL_Renderer * renderer = NULL;
 
 int main(int argc, char* argv[]){
-	SDL_Window * window = NULL;
-	SDL_Renderer * renderer = NULL;
+	int temp = 0;
+	int i = 0;
 	if (SDL_Init(SDL_INIT_VIDEO) > 0){
 		printf("Video Initilization Error:\n%s", SDL_GetError());
 	}
@@ -17,6 +22,12 @@ int main(int argc, char* argv[]){
 			char isRunning = 1;
 			SDL_Event evt;
 			int posX = 100, posY = 100;
+			temp = 0;
+			while (temp < 10){
+				asteroid_add(&asteroid_head);
+				temp++;
+			}
+			temp = 0;
 			while (isRunning){
 				while (SDL_PollEvent(&evt) != 0){
 					if (evt.type == SDL_QUIT)
@@ -28,18 +39,35 @@ int main(int argc, char* argv[]){
 				SDL_RenderClear(renderer);
 				//set the draw colour for our point
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-				//render point
-				SDL_RenderDrawPoint(renderer, posX, posY);
+				//render asteroids
+				render_asteroids();
 				//present point
 				SDL_RenderPresent(renderer);
-
 				posY++;
 				if (posY > 640){ posY = 0; }
 			}
 		}
 	}
+	asteroid_destroy(&asteroid_head);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
+}
+
+int render_asteroids(){
+	int temp = 0;
+	int i = 0;
+	struct Asteroid * a;
+	asteroid_update(&asteroid_head);
+	while (temp < 10){
+		a = asteroid_head;
+		while (i != temp){
+			a = a->next;
+			i++;
+		}
+		i = 0;
+		temp++;
+		SDL_RenderDrawPoint(renderer, a->pos.x, a->pos.y);
+	}
 }
