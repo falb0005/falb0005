443b1190064a3f0658cd224b8bb9bedca178511c
falb0005 <falb0005@algonquinlive.com>
2015-01-25 14:42:53 -0500

    Updated Assignment To Final Implementation

--- Assignment1/Assignment1/GameEngine.cpp
+++ Assignment1/Assignment1/GameEngine.cpp
@@ -1,12 +1,10 @@
 #include "GameEngine.h"
 #include <SDL.h>
-#include <math.h>
 #include "MathUtils.h"
-#include <stdio.h>
 
 GameEngine::GameEngine()
 {
-	
+
 }
 
 GameEngine::~GameEngine()
@@ -18,74 +16,46 @@ void GameEngine::Initialize()
 {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
 
-	window = SDL_CreateWindow("CST8237 Lab",
+	_window = SDL_CreateWindow("CST8237 Lab",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480,
+		640, 640,
 		SDL_WINDOW_SHOWN);
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	_renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
 
-	player.Initialize();
-	
+	InitializeImpl();
 
 	/* Get the time at the beginning of our game loop so that we can track the
 	* elapsed difference. */
-	oldTime = SDL_GetTicks();
-	currentTime = oldTime;
+	_engineTimer.Start();
 }
 
 void GameEngine::Shutdown()
 {
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+	_engineTimer.Stop();
+
+	SDL_DestroyRenderer(_renderer);
+	SDL_DestroyWindow(_window);
 }
 
 void GameEngine::Update()
 {
-	SDL_Event evt;
-	SDL_PollEvent(&evt);
-
 	// Calculating the time difference since our last loop.
-	oldTime = currentTime;
-	currentTime = SDL_GetTicks();
-	deltaTime = (currentTime - oldTime) / 1000;
-	//printf("%f\n", deltaTime);
-	// Check for user input.
-	if (evt.type == SDL_KEYDOWN)
-	{
-		SDL_KeyboardEvent &keyboardEvt = evt.key;
-		SDL_Keycode &keyCode = keyboardEvt.keysym.sym;
-		switch (keyCode)
-		{
-		case SDLK_UP:
-			//pos.y -= (speed * deltaTime);
-			break;
-		case SDLK_DOWN:
-			//pos.y += (speed * deltaTime);
-			break;
-		default:
-			break;
-		}
-	}
+	_engineTimer.Update();
 
-	//update player
-	player.Update(deltaTime);
+	UpdateImpl(_engineTimer.GetDeltaTime());
 }
 
 void GameEngine::Draw()
 {
 	// Set the draw colour for screen clearing.
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255);
 
 	// Clear the renderer with the current draw colour.
-	SDL_RenderClear(renderer);
+	SDL_RenderClear(_renderer);
 
-	// Set the draw colour for our point.
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-	// draw player
-	player.Draw(renderer, deltaTime);
+	DrawImpl(_renderer, _engineTimer.GetDeltaTime());
 
 	// Present what is in our renderer to our window.
-	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(_renderer);
 }
--- Assignment1/Assignment1/GameEngine.h
+++ Assignment1/Assignment1/GameEngine.h
@@ -3,9 +3,12 @@
 // Preprocessor directive to ensure that this header will only be included once. -- Generally used for all environments.
 /*#ifndef _GAME_ENGINE_H_
 #define _GAME_ENGINE_H_
+
 #endif // _GAME_ENGINE_H_*/
-#include "Player.h"
+
 #include "MathUtils.h"
+#include "Player.h"
+#include "Timer.h"
 
 // Forward declaring our renderer and window.
 // Because we're using them as pointers, we don't need to know their size
@@ -29,14 +32,15 @@ public:
 protected:
 	GameEngine();
 
+	virtual void InitializeImpl() = 0;
+	virtual void UpdateImpl(float dt) = 0;
+	virtual void DrawImpl(SDL_Renderer *renderer, float dt) = 0;
+
 	static GameEngine *_instance;
 
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	Player player;
-	
-	// Using the default member-wise initializer for our new struct.
-	float oldTime, currentTime, deltaTime;
-	float speed;
-	float rotationSpeed;
+	SDL_Window *_window;
+	SDL_Renderer *_renderer;
+	Timer _engineTimer;
+
+	float _oldTime, _currentTime, _deltaTime;
 };
